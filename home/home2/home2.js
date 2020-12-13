9581ff0fe238582aa52c85b32c4173ba9a6b2c73
underworld88 <75933852+underworld88@users.noreply.github.com>
2020-12-13 23:56:04 +0800

    Add files via upload

---
+++ home/home2/home2.js
@@ -0,0 +1,13 @@
+// 晚版系统
+Page({
+  todo(){
+    wx.navigateTo({
+      url: '/pages/todo/todo'
+     })
+  },
+  weather(){
+    wx.navigateTo({
+      url: '/pages/weather/weather'
+     })
+  },
+})

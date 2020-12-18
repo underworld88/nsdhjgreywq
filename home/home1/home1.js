45d78f93ec2e1b64089457cd37ef643bdf225f20
underworld88 <75933852+underworld88@users.noreply.github.com>
2020-12-19 07:03:32 +0800

    Add files via upload

---
+++ home/home1/home1.js
@@ -0,0 +1,18 @@
+//早版系统
+Page({
+  tianqi(){
+    wx.navigateTo({
+      url: '/pages/tianqi/tianqi'
+     })
+  }, 
+  calendar(){
+    wx.navigateTo({
+      url: '/pages/index1/index1'
+     })
+  }, 
+  zhihu(){
+    wx.navigateTo({
+      url: '/pages/index/index'
+     })
+  }
+})
